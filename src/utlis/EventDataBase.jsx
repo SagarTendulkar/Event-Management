33114f04ea8c2e625bb7577f3537baec8e21259f
SagarTendulkar <140165649+SagarTendulkar@users.noreply.github.com>
2024-02-07 15:51:56 +0530

    Done

--- src/utlis/EventDataBase.jsx
+++ src/utlis/EventDataBase.jsx
@@ -37,11 +37,56 @@ export const eventList = [
   },
   {
     id: 5,
-    heading: "Epic Comic Con",
-    date: { year: 2024, month: "January" },
-    location: "Mumbai, India",
+    heading: "HorrorFest Expo",
+    date: { year: 2024, month: "October" },
+    location: "Kolkata, India",
     description:
-      "Experience the most anticipated gathering of comic enthusiasts! Epic Comic Con invites you to immerse yourself in a realm where heroes and villains collide. Unveil exclusive collectibles, join panels with renowned artists, and witness legendary cosplayers bring your favorite characters to life. Whether you're a seasoned comic collector or just starting your journey, this event guarantees an epic adventure!",
+      "Experience the spine-chilling terror of HorrorFest! From classic haunted houses to immersive VR experiences, this event is a celebration of all things horror. Encounter iconic monsters, explore eerie attractions, and test your bravery in the face of fear. Whether you're a horror aficionado or a thrill-seeker, HorrorFest guarantees a hair-raising experience you'll never forget!",
+    img: "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    heading: "Anime Game Expo",
+    date: { year: 2025, month: "June" },
+    location: "Chennai, India",
+    description:
+      "Enter the vibrant world of Anime Expo, where anime, manga, and Japanese pop culture collide! Immerse yourself in a kaleidoscope of colorful cosplay, exclusive merchandise, and interactive panels featuring industry luminaries. Whether you're a seasoned otaku or just discovering the magic of anime, Anime Expo promises an unforgettable celebration of Japanese animation and fandom!",
+    img: "https://images.unsplash.com/photo-1502136969935-8d8eef54d77b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    heading: "Board Game Bonanza",
+    date: { year: 2024, month: "January" },
+    location: "Pune, India",
+    description:
+      "Join us for a tabletop extravaganza at Board Game Bonanza! Dive into a world of strategy, luck, and endless fun as you explore a vast array of board games from classic favorites to modern masterpieces. Whether you're a casual player or a seasoned strategist, Board Game Bonanza offers something for everyone. Gather your friends, roll the dice, and embark on an unforgettable gaming adventure!",
+    img: "https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 8,
+    heading: "Steampunk Spectacular",
+    date: { year: 2025, month: "August" },
+    location: "Jaipur, India",
+    description:
+      "Journey into a world of retro-futurism at the Steampunk Spectacular! Explore an alternate history where steam-powered inventions and Victorian aesthetics reign supreme. From elaborate costumes to mesmerizing gadgets, this event celebrates the creativity and ingenuity of the steampunk genre. Whether you're a tinkerer, a historian, or just a curious explorer, Steampunk Spectacular invites you to step back in time and into the future!",
     img: "https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29taWNzfGVufDB8fDB8fHww",
+  },
+  {
+    id: 9,
+    heading: "Wizarding World Expo",
+    date: { year: 2024, month: "December" },
+    location: "Ahmedabad, India",
+    description:
+      "Step into the enchanting realm of the Wizarding World Expo, where magic and wonder await around every corner! Join fellow witches and wizards for a spellbinding celebration of all things magical. From potion-making workshops to Quidditch tournaments, this expo offers a magical experience like no other. Whether you're a Muggle-born or a seasoned wizard, the Wizarding World Expo promises an unforgettable adventure!",
+    img: "https://images.unsplash.com/photo-1502136969935-8d8eef54d77b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 10,
+    heading: "TechCon Spectacular",
+    date: { year: 2025, month: "October" },
+    location: "Bengaluru, India",
+    description:
+      "Explore the cutting edge of technology at TechCon, the ultimate gathering of tech enthusiasts and innovators! Immerse yourself in a world of groundbreaking inventions, futuristic gadgets, and visionary ideas that are shaping the future. From AI and robotics to blockchain and cybersecurity, TechCon showcases the latest trends and developments in the world of technology. Join us as we push the boundaries of innovation and inspire the next generation of tech pioneers!",
+    img: "https://images.unsplash.com/photo-1589254065878-42c9da997008?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
